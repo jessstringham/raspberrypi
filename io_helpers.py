c6fc68de60a260b74aed892397a6419f1cdb3c06
Jessica Stringham <jessstringham@users.noreply.github.com>
2016-11-03 15:12:45 -0700

    Whelp, let's use Popen instead

--- io_helpers.py
+++ io_helpers.py
@@ -1,10 +1,12 @@
-from subprocess import call
+import subprocess
 
 
 def speak(say_wa):
     echo_string = "'{0}'".format(say_wa.replace("'", "'\''"))
 
-    call([
-        "echo", echo_string, "|",
-        "espeak", "-v", "english", "--stdout", "|",
-        "aplay"])
+    echo = subprocess.Popen(['echo', echo_string], stdout=subprocess.PIPE)
+    espeak = subprocess.Popen(["espeak", "-v", "english", "--stdout"],
+                              stdin=echo.stdout, stdout=subprocess.PIPE)
+    echo.stdout.close()
+    subprocess.Popen(['aplay'], stdin=espeak.stdout)
+    espeak.stdout.close()
